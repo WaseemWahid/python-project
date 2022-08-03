e8f77b9ea6387e359a1d60a8cbcb9aefa1e4c6d3
Waseem <wahid.waseem@gmail.com>
2022-08-02 17:03:15 -0700

    deleted player head png and used a better char

--- player.py
+++ player.py
@@ -4,7 +4,7 @@ from settings import *
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, groups, obstacle_sprites):
         super().__init__(groups)
-        self.image = pygame.image.load('player-head.png').convert_alpha()
+        self.image = pygame.image.load('player.png').convert_alpha()
         self.rect = self.image.get_rect(topleft = pos)
         self.hitbox = self.rect.inflate(0, -26)
         #Movement
