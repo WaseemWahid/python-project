b97f10e1cc814ddcb62f72fd4829bd22dae84214
Waseem <wahid.waseem@gmail.com>
2022-08-18 19:21:01 -0700

    monster data added

--- settings.py
+++ settings.py
@@ -25,7 +25,7 @@ HEALTH_COLOR = 'red'
 ENERGY_COLOR = 'blue'
 UI_BORDER_COLOR_ACTIVE = 'gold'
 
-# weapons
+# Weapons
 weapon_data = {
     'sword': {'cooldown': 100, 'damage': 15, 'graphic': './graphics/weapons/sword/full.png'},
     'lance': {'cooldown': 400, 'damage': 30, 'graphic': './graphics/weapons/lance/full.png'},
@@ -34,9 +34,17 @@ weapon_data = {
     'sai': {'cooldown': 80, 'damage': 10, 'graphic': './graphics/weapons/sai/full.png'}
 }
 
-# magic 
+# Magic 
 magic_data = {
     'flame': {'strength': 5, 'cost': 20, 'graphic': './graphics/particles/flame/fire.png'},
     'heal': {'strength': 20, 'cost': 10, 'graphic': './graphics/particles/heal/heal.png'}
+}
+
+# Enemy
+monster_data = {
+    'squid': {'health': 100, 'exp': 100, 'damage': 20, 'attack_type': 'slash', 'attack_sound': './audio/attack/slash.wav', 'speed': 3, 'resistance': 3, 'attack_radius': 80, 'notice_radius': 360},
+    'racoon': {'health': 300, 'exp': 250, 'damage': 40, 'attack_type': 'claw', 'attack_sound': './audio/attack/claw.wav', 'speed': 2, 'resistance': 3, 'attack_radius': 120, 'notice_radius': 400},
+    'spirit': {'health': 100, 'exp': 110, 'damage': 8, 'attack_type': 'thunder', 'attack_sound': './audio/attack/fireball.wav', 'speed': 4, 'resistance': 3, 'attack_radius': 60, 'notice_radius': 350},
+    'bamboo': {'health': 70, 'exp': 120, 'damage': 6, 'attack_type': 'lead_attack', 'attack_sound': './audio/attack/slash.wav', 'speed': 3, 'resistance': 3, 'attack_radius': 50, 'notice_radius': 300},
 }
 
