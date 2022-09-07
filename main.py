9dda217cfd8ccf56a2eaa445a23cc490267c6b3b
Waseem <wahid.waseem@gmail.com>
2022-09-07 12:41:56 -0700

    sounds added and minor bug fixes game completed

--- main.py
+++ main.py
@@ -13,6 +13,11 @@ class Game:
 		pygame.display.set_caption('Zelda')
 		self.clock = pygame.time.Clock()
 		self.level = Level()
+
+		#  sound
+		main_sound = pygame.mixer.Sound('./audio/main.ogg')
+		main_sound.set_volume(0.5)
+		main_sound.play(loops = -1)
         
 	def run(self):
 		while True:
@@ -24,7 +29,7 @@ class Game:
 					if event.key == pygame.K_m:
 						self.level.toggle_menu()
 
-			self.screen.fill('black')
+			self.screen.fill(WATER_COLOR)
 			self.level.run()
 			pygame.display.update()
 			self.clock.tick(FPS)
--- player.py
+++ player.py
@@ -10,7 +10,7 @@ class Player(Entity):
         super().__init__(groups)
         self.image = pygame.image.load('player.png').convert_alpha()
         self.rect = self.image.get_rect(topleft = pos)
-        self.hitbox = self.rect.inflate(0, -26)
+        self.hitbox = self.rect.inflate(-6, HITBOX_OFFSET['player'])
 
         #Graphic setup
         self.import_player_assests()
@@ -52,6 +52,10 @@ class Player(Entity):
         self.hurt_time = None
         self.invulnerability_duration = 500
 
+        # import a soung 
+        self.weapon_attack_sound = pygame.mixer.Sound('./audio/sword.wav')
+        self.weapon_attack_sound.set_volume(0.4)
+
     def import_player_assests(self):
         character_path = './graphics/player/'
         self.animations = {
@@ -92,6 +96,7 @@ class Player(Entity):
                 self.attacking = True
                 self.attack_time = pygame.time.get_ticks()
                 self.create_attack()
+                self.weapon_attack_sound.play()
 
             # magic input (left control is for magic)
             if keys[pygame.K_LCTRL]:
